fe9c7e666f62f741052e4857ed594f4d8a675dba
wangzhenxyz <wangzhen@flyaudio.cn>
2015-01-08 15:50:40 +0800

    ------------------------------------------------------------

---
+++ MediaPlayer（车技代码）/gen/com/flyAudio/flyMediaPlayer/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.flyAudio.flyMediaPlayer;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ MediaPlayer（车技代码）/src/com/flyaudio/flyMediaPlayer/perferences/CoverList.java
@@ -0,0 +1,11 @@
+package com.flyaudio.flyMediaPlayer.perferences;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+
+
+public class CoverList {
+
+	public static Drawable cover;
+
+}
